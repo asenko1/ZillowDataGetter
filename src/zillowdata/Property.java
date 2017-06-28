3b8601944eb7cc040463403a473065a632351b7e
asenko1 <alex.senko598@gmail.com>
2017-06-28 14:42:41 -0700

    adding changes to source

--- src/zillowdata/Property.java
+++ src/zillowdata/Property.java
@@ -1,15 +1,45 @@
 package zillowdata;
 
 public class Property {
-	private String address;
-	private String city;
-	private String state;
-	private	String zipcode;
-	
+	private Address address;
 	private ZillowID id;
 	private ZillowData data;
 	
+	public Property(String street, String CityandState, String zipcode){
+		address = new Address(street, CityandState, zipcode);
+		
+	}
 	
+	private void getID(){
+		id = new ZillowID();
+	}
 	
-
+	private void getData(){
+		
+	}
+	
 }
+
+class Address{
+	String street;
+	String CityandState;
+	String zipcode;
+	
+	public Address(String street, String CityandState, String zipcode){
+		this.street = street;
+		this.CityandState = CityandState;
+		this.zipcode = zipcode;
+	}
+	
+	public String getStreet(){
+		return street;
+	}
+	public String getCityandState(){
+		return CityandState;
+	}
+	public String getZipcode(){
+		return zipcode;
+	}
+	
+}
+
